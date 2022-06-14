85b3994742be2a35dbe28203c7e6286faf62bbc6
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-14 22:32:26 +0300

    Add task019

---
+++ task019/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Enter five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("----------");
+
+
+/*
+Console.WriteLine("second " + number / 1000 % 10); // вторая
+Console.WriteLine("first " + number / 10000); // первая цифра
+Console.WriteLine("last " + number % 10); // последняя
+Console.WriteLine("fourth " + number / 10 % 10); // предпоследняя
+*/
+
+
+if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
+{
+    Console.WriteLine("The number is polindrome");
+}
+else if (number > 9999 && number < 100000)
+{
+    Console.WriteLine("The number is NOT polindrome");
+}
+else
+{
+    Console.WriteLine("Your number is out of range");
+}
