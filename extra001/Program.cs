b120dbdc9f6fb88496a2cffc696881ba07b17da2
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-15 20:21:43 +0300

    Add extra task 001

---
+++ extra001/Program.cs
@@ -0,0 +1,23 @@
+﻿// В переменной string есть секретное сообщение, во второй есть пароль. 
+// Пользователю программы даётся 3 попытки ввести пароль и увидеть 
+// секретное сообщение.
+
+string text = "Secret message";
+string password = "Password";
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Enter password: ");
+    string input = Console.ReadLine();
+    Console.WriteLine("------------------");
+    if (input == password)
+    {
+        Console.WriteLine(text);
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong password");
+        Console.WriteLine();
+    }
+}
