2a7aa0cbbebba93b9ddc35ba09e8e7714921e0a5
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-15 20:33:13 +0300

    Add extra task 002

---
+++ extra002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit 
+// программа заканчивается
+
+for (int i = 1; i > 0; i++)
+{
+    Console.WriteLine(i);
+    string input = Console.ReadLine();
+    string text = "exit";
+    if (input == text)
+    {
+        break;
+    }
+}
