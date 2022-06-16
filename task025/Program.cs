15eeeaba7cc7786ca993ea7e2489278983f64dc3
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-16 23:21:00 +0300

    Add task025

---
+++ task025/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Enter two numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i = 1; i <= b; i++)
+{
+    result = result*a;
+}
+Console.WriteLine($"a^b = {result}");
