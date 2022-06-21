5f8c4f9722dc008eadb0a28a6c4cfc2b21c38da8
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-21 22:06:29 +0300

    Add task 038 - Find difference between max and min elements of array

---
+++ task038/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] array = new int [5];
+FillRandomArray(array);
+PrintArray(array);
+
+int min = array[0];
+int max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (min > array[i]) min = array[i];
+    else if (max < array[i]) max = array[i];
+}
+int result = max - min;
+
+Console.WriteLine($"The difference between max and min array elements = {result}");
+
+void FillRandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99,100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
