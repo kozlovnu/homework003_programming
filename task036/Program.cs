0fa0fe4fb4460514b3f8bec38ea0467d770f70ca
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-21 21:55:51 +0300

    Add task 036 Find sum of odd array elements

---
+++ task036/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] array = new int[4];
+FillRandomArray(array);
+PrintArray(array);
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0) sum += array[i];
+}
+Console.WriteLine($"Sum of odd array elements = {sum}");
+
+
+void FillRandomArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99,100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i =0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
