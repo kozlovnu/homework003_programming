a8b106195dddb92fba10b2b01c0ce87551b5408a
Nikita Kozlov <kozlov.nu@gmail.com>
2022-07-09 20:29:49 +0300

    Add task056 Find a line with smallest sum of elements

---
+++ task056/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+int[,] array = new int[5,3];
+int[] sum = new int[array.GetLength(0)];
+FillRandomArray(array);
+PrintArray(array);
+FindSum(array,sum);
+
+void FindSum(int[,] array, int[] sum)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum[i] += array[i,j];
+        }
+        Console.WriteLine($"Sum of elements line {i} = {sum[i]}");
+    }
+    int min = sum[0];
+    int index = 0;
+    for (int i = 0; i < sum.Length; i++)
+    {
+        if (min > sum[i])
+        {
+            min = sum[i];
+            index = i;
+        }
+        
+    }
+    Console.WriteLine($" Line {index} is the line with the smallest sum of elements");
+}
+
+void FillRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
