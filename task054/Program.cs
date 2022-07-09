35eae894b28e322388a7e7b047db9fbd085eef8c
Nikita Kozlov <kozlov.nu@gmail.com>
2022-07-09 19:48:50 +0300

    Add task054 Sort Array

---
+++ task054/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] array = new int[5,5];
+int[,] result = new int[5,5];
+FillRandomArray(array);
+PrintArray(array);
+Console.WriteLine("Sorted array: ");
+SortLine(array);
+PrintArray(array);
+
+void SortLine(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j; k < array.GetLength(1); k++)
+            {
+                if (array[i,j] <= array[i,k]) 
+                {
+                    int temp = array[i,j];
+                    array[i,j] = array[i,k];
+                    array[i,k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void FillRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
